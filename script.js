79e8d2f4468673326c77cae0d8ac9629b72be9cc
fakegithubprofile <signinmekaamaayega@gmail.com>
2024-09-16 19:13:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-console.log("Hi!");
-
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
 themeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  themeToggle.textContent = body.classList.contains('dark-mode') ? '🌙' : '☀️';
+  body.classList.toggle('dark');
+  themeToggle.textContent = body.classList.contains('dark') ? '🌙' : '☀️';
 });
